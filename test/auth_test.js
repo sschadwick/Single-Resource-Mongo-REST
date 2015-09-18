5b19892394c12b0add5c964906f79415f57c5671
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-18 13:54:36 -0700

    unique username test

--- test/auth_test.js
+++ test/auth_test.js
@@ -46,6 +46,21 @@ describe('auth', function() {
     });
   });
 
+  describe('username must be unique', function() {
+    it('should return an error', function(done) { //add new user to database before tests
+      var user = new User();
+      user.username = 'testuser1'; //this user should already exist according to signup route
+      user.basic.username = 'testuser1';
+      user.generateHash('foobar123', function(err, res) {
+        if (err) throw err;
+        user.save(function(err, data) {
+          expect(err.code).to.eql(11000); //err code key already exists in db
+          done();
+        }.bind(this));
+      }.bind(this));
+    });
+  });
+
   describe('user info in database', function() {
     before(function(done) { //add new user to database before tests
       var user = new User();
