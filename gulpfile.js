7b48f76b08e933b799a9086389558184471ad1ea
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-19 11:45:43 -0700

    added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var gulpMocha = require('gulp-mocha');
+var watch = require('gulp-watch');
 
-var filesToWatch = ['server.js', '/routes/*.js', 'models/*.js', 'lib/*.js', 'test/**/*.js', 'gulpfile.js'];
+var filesToWatch = ['server.js', 'routes/*.js', 'models/*.js', 'lib/*.js', 'test/**/*.js', 'gulpfile.js'];
 
 gulp.task('jshint', function() {
   return gulp.src(filesToWatch)
@@ -15,6 +16,10 @@ gulp.task('jshint', function() {
 gulp.task('test', function() {
   return gulp.src('test/**/*test.js')
     .pipe(gulpMocha({reporter: 'nyan'}));
+}); 
+
+gulp.task('watch', function() {
+  return gulp.watch(filesToWatch, ['default']);
 });
 
 gulp.task('default', ['jshint', 'test']);
